263ae6a54bab997d123e9b2e947841a181988a67
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-03-16 20:52:09 +0530

    Added Linear Search solution

---
+++ Arrays/Linear_Search.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int findKey(int arr[], int n, int key){
+
+    for(int i=0;i<n;i++){
+        
+        if(arr[i]==key){
+            return i;
+        }
+
+    }
+
+    return -1;
+}
+
+int main(){
+    
+    int n;
+
+    cout << "Enter number of elements : ";
+    cin >> n;
+
+    int key;
+
+    cout << "Enter key to search : ";
+    cin >> key;
+
+    int arr[n];
+
+    cout << "Enter elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    int result = findKey(arr,n,key);
+
+    cout << "Given Key is in : " << result << endl;
+    
+    return 0;
+
+}
