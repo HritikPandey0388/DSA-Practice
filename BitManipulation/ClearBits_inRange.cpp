85a9ef53b8950ec8086ccfdb03d8c95a612589c2
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-03-27 18:28:32 +0530

    Added clearing i-jth Bitsin range code

---
+++ BitManipulation/ClearBits_inRange.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void ClearBits(int Num, int i, int j){                    
+ 
+    int left = (1 << (j+1));                   // making left Bits 
+    int right = ~(~(0) << i);                  // making right Bits 
+
+    int BitMask = (left | right);              //desired bitmask 
+
+    Num = (Num & BitMask);
+
+    cout << ans << endl;
+
+}
+
+int main(){
+
+    ClearBits(31,2,3);
+
+    return 0;
+}
