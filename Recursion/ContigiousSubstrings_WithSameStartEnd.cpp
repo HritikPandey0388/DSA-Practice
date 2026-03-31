c10b264ff3a3217b7db4e2193585a5e49a9c2db4
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-03-31 12:51:47 +0530

    Added Contigious Substrings of a string with same charater as start and end code

---
+++ Recursion/ContigiousSubstrings_WithSameStartEnd.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void AllSubstrings(string &str, int st, int end){
+
+    int n = str.size();
+
+    if(st==n){
+        return ;
+    }
+
+    if(end==n){
+        AllSubstrings(str,st+1,st+1);
+        return;
+    }
+
+    if(str[st]==str[end]){
+        cout << str.substr(st,end-st+1) << endl;
+    }
+
+    AllSubstrings(str,st,end+1);
+
+}
+
+int main(){
+
+    string str = "abcab";
+    string ans = "";
+    AllSubstrings(str,0,0);
+
+    return 0;
+}
