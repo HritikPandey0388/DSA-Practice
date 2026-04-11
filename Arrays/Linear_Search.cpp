124f74fa1304c7f564c415cad83f39c33435515a
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-04-11 21:04:35 +0530

    Added comments in code

--- Arrays/Linear_Search.cpp
+++ Arrays/Linear_Search.cpp
@@ -1,50 +1,24 @@
-/*
-    Problem: Linear Search in an Array
-    Author: Hritik Pandey
-    Description:
-    This program takes n numbers and a key as input,
-    and searches for the key in the array using linear search.
-    It prints the index if found, or -1 if not found.
-
-    Time Complexity: O(n)
-    Space Complexity: O(1)
-*/
-
 #include<iostream>
 using namespace std;
 
-int findKey(int arr[], int n, int key){
+int findKey(int *arr, int n, int key){
 
     for(int i=0;i<n;i++){
         
         if(arr[i]==key){
-            return i;
+            return i;   // return index when key is found
         }
 
     }
 
-    return -1;
+    return -1;   // return -1 if key not present
 }
 
 int main(){
-    
-    int n;
 
-    cout << "Enter number of elements : ";
-    cin >> n;
-
-    int key;
-
-    cout << "Enter key to search : ";
-    cin >> key;
-
-    int arr[n];
-
-    cout << "Enter elements : ";
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
-    }
-
+    int arr[7] = {1,5,3,-4,0,8,9};
+    int key = 9;
+    int n = 7;
     int result = findKey(arr,n,key);
 
     cout << "Given Key is in : " << result << "th Index" << endl;
