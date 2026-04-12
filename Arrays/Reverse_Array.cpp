f4f576e070d5635a9b9e223fbbce19a06dee1c83
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-04-12 17:03:00 +0530

    Added comments in code

--- Arrays/Reverse_Array.cpp
+++ Arrays/Reverse_Array.cpp
@@ -1,34 +1,25 @@
-/*
-    Problem: Reverse an Array
-    Author: Hritik Pandey
-    Description:
-    This program takes n numbers as input and reverses
-    the array in-place. It then prints the reversed array.
-
-    Time Complexity: O(n)
-    Space Complexity: O(1)
-*/
-
 #include<iostream>
 using namespace std;
 
-void ReverseArray(int arr[], int n){
+void ReverseArray(int* arr, int n){
 
-    int st=0, end=(n-1);
+    int st=0, end=(n-1);                 
 
     while(st<end){
 
-        int temp = arr[st];
+        // reversing arr[st] and arr[end]
+        int temp = arr[st];               
         arr[st] = arr[end];
         arr[end] = temp;
 
-        st++;
-        end--;
+        // updating 
+        st++;       // st(-->) 
+        end--;      // end(<--)
     }
 
 }
 
-int PrintArray(int arr[], int n){
+int PrintArray(int* arr, int n){
 
     for(int i=0;i<n;i++){
         cout << arr[i] << " " ;
@@ -41,17 +32,8 @@ int PrintArray(int arr[], int n){
 
 int main(){
     
-    int n;
-
-    cout << "Enter number of elements : ";
-    cin >> n;
-
-    int arr[n];
-
-    cout << "Enter elements : ";
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
-    }
+    int arr[5] = {1,2,3,4,5};
+    int n = 5;  
 
     cout << " Given Array is : " ;
     PrintArray(arr,n);
