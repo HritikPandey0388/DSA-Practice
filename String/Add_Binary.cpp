9670daea1dbe92adfb7abc38a02de58863be9ec8
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-04-13 20:06:03 +0530

    Added addition of 2 Binary numbers code

--- String/Add_Binary.cpp
+++ String/Add_Binary.cpp
@@ -7,8 +7,9 @@ void AddBinary(string &nums1, string &nums2){
 
     int carry=0;
     int i=nums1.size()-1, j=nums2.size()-1, idx=0;
-    string ans = "";
+    string ans = "";                                  // ans
 
+    // Continue until both strings are exhausted AND no carry remains
     while(i>=0 || j>=0 || carry){
         
         int x=0, y=0, sum=0;
@@ -21,15 +22,16 @@ void AddBinary(string &nums1, string &nums2){
             y = nums2[j]-'0';
         }
 
-        sum = x + y + carry;
-        ans = to_string(sum%2) + ans;
-        carry = sum/2 ;
+        sum = x + y + carry;                  // sum = 0,1,2
+        ans = to_string(sum%2) + ans;         // finding last Bit(0,1)   
+        carry = sum/2 ;                       // checking carry exist or not
 
         i--;
         j--;
 
     }
 
+    // if a carry remains after both strings are processed
     if(carry){
         ans.insert(ans.begin(),1);
     }
@@ -42,10 +44,10 @@ void AddBinary(string &nums1, string &nums2){
 
 int main(){
 
-    string nums1= "1111";
-    string nums2= "111";
+    string nums1 = "1111";   // binary for 15
+    string nums2 = "111";    // binary for 7
 
-    AddBinary(nums1,nums2);
+    AddBinary(nums1, nums2); // expected output: 10110 (binary for 22)
 
     return 0;
 }
