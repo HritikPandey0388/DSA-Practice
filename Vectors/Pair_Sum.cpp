0026e15b3759175837c758534bd4c70a9fcc0987
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-04-14 22:27:07 +0530

    Added added pseudocode in code

--- Vectors/Pair_Sum.cpp
+++ Vectors/Pair_Sum.cpp
@@ -5,22 +5,25 @@ using namespace std;
 void Pairs(vector<int> &nums, int target){
 
     int n = nums.size();
-    int st = 0 ,end = (n-1);
+    int st = 0 ,end = (n-1);  // starting and ending index 
     int currSum = 0;
-    vector<int> ans;
+    vector<int> ans;          // contains ans pair
 
     while(st<end){
 
         currSum = nums[st]+nums[end];
 
+        // pairs obtained
         if(currSum==target){
             ans.push_back(st);
             ans.push_back(end);
             break;
         }
+        // excluding nums[st]
         else if(currSum<target){
             st++;
         }
+        // excluding nums[end]
         else{
             end--;
         }
