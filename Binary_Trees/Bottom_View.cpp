5e24a17a12c3d3cedb1d873d85f79eea336a6af6
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-04-17 23:59:47 +0530

    Added Bottom View of Binary Tree code

---
+++ Binary_Trees/Bottom_View.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+    
+};
+
+static int idx = -1;
+
+Node* BuildTree(vector<int> nodes){
+    
+    idx++;
+
+    if(nodes[idx]==-1){
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);             // creating new node
+    currNode->left = BuildTree(nodes);                 // left subTree
+    currNode->right = BuildTree(nodes);                // right subTree
+    
+    return currNode;
+}
+
+void BottomView(Node* root){
+
+    queue<pair<Node*,int>> q;
+    map<int,int> m;
+
+    q.push(make_pair(root,0));
+
+    while(!q.empty()){
+
+        pair<Node*,int> curr = q.front();
+        q.pop();
+
+        Node* currNode = curr.first;
+        int currHD = curr.second;
+
+        // if(m.count(currHD)==0){
+        //     m[currHD] = currNode->data;
+        // }
+        
+        m[currHD] = currNode->data;
+
+        if(currNode->left!=NULL){
+            pair<Node*,int> left = make_pair(currNode->left,currHD-1); 
+            q.push(left);
+        }
+        if(currNode->right!=NULL){
+            pair<Node*,int> right = make_pair(currNode->right,currHD+1); 
+            q.push(right);
+        }
+
+    }
+
+    for(auto it:m){
+        cout << it.second << " " ;
+    }
+
+    cout << endl;
+    
+}
+
+int main(){
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+    Node* root = BuildTree(nodes);
+
+    BottomView(root);
+
+    return 0;
+}
