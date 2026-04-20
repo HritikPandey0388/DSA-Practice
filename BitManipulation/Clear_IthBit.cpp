213f22ac1a1ff1131ff7d6bd2a25806d0ed3e228
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-04-20 17:22:38 +0530

    Added clearing Ith Bit of a given number code

--- BitManipulation/Clear_IthBit.cpp
+++ BitManipulation/Clear_IthBit.cpp
@@ -3,12 +3,10 @@ using namespace std;
 
 int Clear_ithBit(int n, int i){
 
-    int BitMask = ~(1 << i);
+    int BitMask = ~(1 << i);  // mask with 0 at i-th position
 
-    return (n & BitMask);
-
+    return (n & BitMask);     // clearing i-th bit
 }
-
 int main(){
 
     cout << Clear_ithBit(6,1) << endl;
