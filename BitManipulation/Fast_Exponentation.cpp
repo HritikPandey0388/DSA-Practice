19bad36d15890015f5ce614e129d5802efe69a96
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-04-21 09:54:22 +0530

    Addeda raise to power b code

--- BitManipulation/Fast_Exponentation.cpp
+++ BitManipulation/Fast_Exponentation.cpp
@@ -1,28 +1,33 @@
 #include<iostream>
 using namespace std;
 
-void FastExponentation(int base, int pow){                   
- 
-    int ans = 1;
+void FastExponentation(int base, int pow){
 
-    while(pow>0){
+    int ans = 1;  
 
+    while(pow > 0){
+
+        // lastBit --> 0/1
         int lastDig = pow & 1;
+
+        // If 1 --> multiply current base with answer
         if(lastDig){
             ans *= base;
         }
 
+        // Square the base for next iteration
         base *= base;
+
+        // updating power (divide by 2)
         pow = pow >> 1;
     }
 
     cout << ans << endl;
-
 }
 
 int main(){
 
-    FastExponentation(3,5);
+    FastExponentation(3,5);   // 3^5 = 243
 
     return 0;
 }
