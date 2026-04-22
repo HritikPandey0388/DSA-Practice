10d61786f52ebf25f52d89136f4b801034023211
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-04-22 19:11:50 +0530

    Added Single number in given array code

--- BitManipulation/Single_Number.cpp
+++ BitManipulation/Single_Number.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int singleNumber(vector<int>& nums) {
 
-    int ans=0;
+    int ans = 0;  
 
     for(int value : nums){
-        ans^=value;
+        ans ^= value;          // Duplicate numbers will cancel each other
     }
     
-    return ans;
+    return ans; 
 }
 
 int main(){
     
-    vector<int> nums = {1,2,2,3,3,3,4,4};
+    vector<int> nums = {1,2,2,3,3,4,4};   // only works when all numbers are 2 times present
     cout << singleNumber(nums) << endl;
 
     return 0;
