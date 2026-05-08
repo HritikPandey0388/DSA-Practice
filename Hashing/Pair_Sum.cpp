fd48c2ee5d0c03eb65b14506bed7fc3509b5934f
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-05-09 00:58:40 +0530

    Added PAir Sum equals to given target code

---
+++ Hashing/Pair_Sum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main(){
+
+    int arr[7] = {1, 2, 7, 11, 15, 5, 9};
+    int n = 7;
+    int target = 9;
+
+    unordered_map<int, int> m;
+
+    for(int i=0; i<n; i++){
+
+        // req. number
+        int comp = target - arr[i];
+
+        // req. Number --> Exists
+        if(m.count(comp)){
+            cout << "ans : " << m[comp] << " , " << i << endl;
+            break;
+        }
+
+        // push current number
+        m[arr[i]] = i;
+    }
+
+    return 0;
+}
