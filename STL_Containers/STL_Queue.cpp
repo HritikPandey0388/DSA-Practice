fd63564248cb090ad6e0642a4e2d34eb528d3509
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-05-10 11:52:06 +0530

    Added Queue using STL code

--- STL_Containers/STL_Queue.cpp
+++ STL_Containers/STL_Queue.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 int main(){
 
+    // initializing STL queue
     queue<int> q;
 
+    // push
     for(int i=0;i<5;i++){
         q.push(i);
     }
 
+    // accessing each element
     while(!q.empty()){
         cout << q.front() << "-->";
         q.pop();
