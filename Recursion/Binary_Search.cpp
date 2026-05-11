426fb872978497bae6d7b9b7a9b055261f697d18
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-05-12 00:58:18 +0530

    Added Binary Search code

--- Recursion/Binary_Search.cpp
+++ Recursion/Binary_Search.cpp
@@ -6,20 +6,25 @@ int BinarySearch(vector<int> &nums, int st, int end, int target){
 
     if(st<end){
 
+        // find mid
         int mid = st + (end-st)/2;
 
+        // target found
         if(nums[mid]==target){
             return mid;
         }
+        // target exist in RHS
         else if(nums[mid]<target){
             return BinarySearch(nums,mid+1,end,target);
         }
+        // target exist in LHS
         else{
             return BinarySearch(nums,st,mid-1,target);
         }
 
     }
 
+    // doesn't exist
     return -1;
 }
 
