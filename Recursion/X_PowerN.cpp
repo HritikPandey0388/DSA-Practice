3e39aca824ad7edac2684675574d7d51f4638021
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-05-13 20:34:48 +0530

    Added a raise to the power b code

--- Recursion/X_PowerN.cpp
+++ Recursion/X_PowerN.cpp
@@ -3,19 +3,22 @@ using namespace std;
 
 int Power(int a, int b){
 
+    // base case:
     if(b==0){
         return 1;
     }
 
+    // recursively calculate a^(b/2)
     int ans = Power(a,b/2);
 
+    // exponent --> odd:
     if(b%2!=0){
         return a*ans*ans;
     }
 
+    // exponent --> even
     return ans*ans;
 }
-
 int main(){
     
     int ans = Power(2,7);
