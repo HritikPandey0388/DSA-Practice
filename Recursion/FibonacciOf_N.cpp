1147fe9d9fa5beb9de1ef2b7d867e17aa566d321
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-05-15 01:38:50 +0530

    Added Fibonacci of any given number n code

--- Recursion/FibonacciOf_N.cpp
+++ Recursion/FibonacciOf_N.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int fibonacci(int n){
 
+    // base case
     if(n==0 || n==1){
         return n;
     }
 
+    // fibonacci of nth term -- > sum of last 2 terms (n-1 + n-2)
     return fibonacci(n-1)+fibonacci(n-2);
 }
 
