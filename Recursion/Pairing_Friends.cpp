560e3b13bc7bc576ec4d109d3f2fa88a07149322
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-05-14 23:22:23 +0530

    Added how many ways n peoples can be paired code

--- Recursion/Pairing_Friends.cpp
+++ Recursion/Pairing_Friends.cpp
@@ -3,14 +3,26 @@ using namespace std;
 
 int Pairing(int n){
 
+    // Base cases:
+    // 1 Person --> stays single
+    // 2 Person --> both make pair or both stands single
     if(n==1 || n==2){
         return n;
     }
 
-    return Pairing(n-1) + (n-1)*Pairing(n-2);
+    // Case 1:
+    // nth person stays single
+    int single = Pairing(n-1);
+
+    // Case 2:
+    // nth person pairs with any one of the remaining (n-1) people
+    // reamaining make pairs
+    int pairUp = (n-1) * Pairing(n-2);
+
+    // Total ways = single ways + pairing ways
+    return single + pairUp;
 
 }
-
 int main(){
 
     int n = 4;
