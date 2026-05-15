f1b0bee39627703006dac66d118c30073ac90c08
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-05-16 02:16:05 +0530

    Added all substrings of a string with same first and last character code

--- Recursion/ContigiousSubstrings_WithSameStartEnd.cpp
+++ Recursion/ContigiousSubstrings_WithSameStartEnd.cpp
@@ -2,22 +2,31 @@
 using namespace std;
 
 void AllSubstrings(string &str, int st, int end){
-
+    
     int n = str.size();
 
+    // Base case
     if(st==n){
         return ;
     }
 
+    // If ending index --> end,
+    // move to next starting index
     if(end==n){
+
+        // Start new substring from next position
         AllSubstrings(str,st+1,st+1);
         return;
     }
 
+    // first character = last character
     if(str[st]==str[end]){
+
+        // Print substring 
         cout << str.substr(st,end-st+1) << endl;
     }
 
+    // Expand current substring --> end+1
     AllSubstrings(str,st,end+1);
 
 }
