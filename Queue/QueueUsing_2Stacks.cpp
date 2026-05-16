740fcbf4d520de9ed625cdf0b593f1cb1c7245fe
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-05-17 01:29:30 +0530

    Added Implementation of Queue using 2 Stacks code

--- Queue/QueueUsing_2Stacks.cpp
+++ Queue/QueueUsing_2Stacks.cpp
@@ -4,36 +4,49 @@ using namespace std;
 
 template<typename T>
 class Queue{
-    stack<T> s1;
-    stack<T> s2;
+
+    stack<T> s1;   // main stack
+    stack<T> s2;   // Temporary stack
 
 public:
 
+    // Insert element
     void push(T data){
 
+        // Move all elements from s1 to s2
         while(!s1.empty()){
+
             s2.push(s1.top());
             s1.pop();
         }
 
+        // Insert new element in s1
         s1.push(data);
 
+        // Move all elements from s2 to s1
         while(!s2.empty()){
+
             s1.push(s2.top());
             s2.pop();
         }
 
     }
 
+    // Remove front element
     void pop(){
+
         s1.pop();
     }
 
+    // front element
     T front(){
+
         return s1.top();
     }
 
+    // empty
     bool empty(){
+
         return s1.empty();
     }
 
