4501b593c70abdcacb050cd03aef0d38bde6176e
HritikPandey0388 <pandeyhritik0388@gmail.com>
2026-05-20 23:51:55 +0530

    Added Adding 2 number as LL input code

--- LinkedList/AddTwo_Numbers.cpp
+++ LinkedList/AddTwo_Numbers.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Defining singly-linked list
 struct ListNode {
     int val;
     ListNode* next;
@@ -11,48 +10,78 @@ struct ListNode {
     ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
-// Function to add 2 LinkedList
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+    // Dummy node for result linked list
     ListNode* l3 = new ListNode(0);
+
+    // Carry generated during addition
     int carry = 0;
+
+    // store head for new LL
     ListNode* head = l3;
 
+    // Traverse both linked lists together
     while (l1 && l2) {
+
+        // Add current digits + previous carry
         int value = l1->val + l2->val + carry;
+
+        // Update carry
         carry = value / 10;
+
+        // Store current digit in result list
         l3->next = new ListNode(value % 10);
 
+        // Move all pointers forward
         l1 = l1->next;
         l2 = l2->next;
         l3 = l3->next;
     }
 
+    // If elements remain in l1
     while (l1) {
+
+        // Add remaining digit with carry
         int value = l1->val + carry;
+
+        // Update carry
         carry = value / 10;
+
+        // Store current digit
         l3->next = new ListNode(value % 10);
 
+        // Move forward
         l1 = l1->next;
         l3 = l3->next;
     }
 
+    // If elements remain in l2
     while (l2) {
+
+        // Add remaining digit with carry
         int value = l2->val + carry;
+
+        // Update carry
         carry = value / 10;
+
+        // Store current digit
         l3->next = new ListNode(value % 10);
 
+        // Move forward
         l2 = l2->next;
         l3 = l3->next;
     }
 
+    // If carry still remains
     if (carry) {
         l3->next = new ListNode(carry);
     }
 
+    // Return actual head
     return head->next;
 }
 
-// Helper function to print list
 void printList(ListNode* head) {
     while (head) {
         cout << head->val << " -> ";
@@ -61,7 +90,7 @@ void printList(ListNode* head) {
     cout << "NULL\n";
 }
 
-// Helper to create list from array
+// create list from array
 ListNode* createList(int arr[], int n) {
 
     ListNode* head = new ListNode(arr[0]);
